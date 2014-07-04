8bbf76dc7cc0ffde25443a13b456cca56032e311
jrior001 <jriordan001@gmail.com>
2014-07-04 11:10:19 -0400

    Package with current CM ville kernel w/F2FS support

--- kernel/mkbootimg.sh
+++ kernel/mkbootimg.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 echo \#!/sbin/sh > /tmp/createnewboot.sh
-echo /tmp/mkbootimg --kernel /tmp/boot.img-zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x$(cat /tmp/boot.img-base) --pagesize 2048 --ramdiskaddr 0x82000000 --output /tmp/newboot.img >> /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x$(cat /tmp/boot.img-base) --pagesize 2048 --ramdiskaddr 0x82000000 --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
 return $?
